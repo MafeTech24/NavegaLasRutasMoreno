d4673712d12786d381de737184e80a810034cc80
“Mafe” <mafe4024@gmail.com>
2025-06-17 19:36:25 -0300

    mejora2

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,18 +1,26 @@
 import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import ItemList from './ItemList';
 
 
 
   
 function ItemListContainer() { 
+
   const [items, setItems] = useState([])
+  const { categoryName } = useParams();
   
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
       .then((res) => res.json())
       .then((data) => setItems(data))
       .catch((err) => console.error('Error fetching products:', err));
-  }, []);
+    
+      fetch(`https://fakestoreapi.com/products/category/${categoryName}`)
+      .then(res => res.json())
+      .then(data => setProductos(data))
+      .catch(err => console.error('Error al cargar productos:', err));
+  }, [categoryName]);   
 
 
 return (
