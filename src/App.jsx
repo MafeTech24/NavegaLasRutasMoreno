49b1ccee931a56de1f3e83795e1b2bfc6d148621
“Mafe” <mafe4024@gmail.com>
2025-07-05 21:53:31 -0300

    se corrige ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import NavBar from './Components/NavBar';
+import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from "react-router";
